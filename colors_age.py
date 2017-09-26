0f96f62a3b3bf168ee6bc22fa20404de75bd9458
hr <hans@rieke.us>
2017-09-26 15:51:47 -0400

    just test code

---
+++ colors_age.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on 9/26/17, 3:48 PM
+
+@author: hansrieke
+Includes: """
+
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on 9/26/17, 3:19 PM
+
+@author: hansrieke"""
+
+colors = ["red", "yellow", "green", "blue"]
+
+i = 0
+while i < len(colors):
+    print "When I was %d, my favorite color was %s" %(i, colors[i])
+    i = i + 1
